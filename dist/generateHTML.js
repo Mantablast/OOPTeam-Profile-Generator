49a77d63def9c9bc9605955a60e4bcea5b17276c
AimeeJay <ajesso@post.com>
2021-04-27 20:10:11 -0400

    questions functional again

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -6,8 +6,21 @@ const Intern = require('../lib/Intern')
 
 
 //Where the html will be generated
-function generateHtml() {
-
+function generateHtml(data) {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Team</title>
+</head>
+<body>
+    <h1>Manager Name : ${data.name}</h1>
+</body>
+</html>
+    `;
 }
 
 
